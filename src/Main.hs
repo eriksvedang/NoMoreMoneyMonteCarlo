2b7f3c53a18af2d88acbe4aac5e0645f40db1bcf
Erik Svedäng <erik.svedang@gmail.com>
2015-01-21 16:01:45 +0100

    more

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import Control.Monad
 import Control.Monad.State
 import System.Random
 import Data.List
+import Numeric
 
 data GameInfo = GameInfo {
   gameRound   :: Integer, -- how many rounds of the game has been played
@@ -17,45 +18,59 @@ mkGame = GameInfo {
   gameDanger = 1
 }
 
+data GameSettings = GameSettings {
+  settingsCrashLimit :: Integer,
+  settingsDangerIncrease :: Bool
+}
+
 main = do
   let runs = 1000
-  putStrLn "NO MORE MONEY"
-  printExperiment runs 4
-  printExperiment runs 5
+  putStrLn $ "NO MORE MONEY, " ++ show runs ++ " runs"
+  printExperiment runs (GameSettings 4 False)
+  printExperiment runs (GameSettings 5 False)
+  printExperiment runs (GameSettings 4 True)
+  printExperiment runs (GameSettings 5 True)
 
-printExperiment :: Int -> Integer -> IO ()
-printExperiment runs crashLimit = do
+printExperiment :: Int -> GameSettings -> IO ()
+printExperiment runs settings = do
   g <- newStdGen
-  let gameLengths = experiment g runs crashLimit
+  let gameLengths = runExperiment g runs settings
   let average = (fromIntegral (sum gameLengths)) / (fromIntegral runs)
   let freqs = (sort (frequencies gameLengths))
-  putStrLn "-------------------------------------------------"
-  putStrLn ("Using " ++ show crashLimit ++ " crashes, game will average " ++ show average ++ " rounds.\n")
-  putStrLn "Rounds \t\tFrequencies\tPercentage"
+  putStrLn "\n-------------------------------------------------\n"
+  putStrLn ("Using " ++ show (settingsCrashLimit settings) ++ " crashes" ++
+           (if (settingsDangerIncrease settings) then " and increasing danger level" else "") ++
+           ", game will average " ++ show average ++ " rounds:\n")
+  putStrLn "Rounds \t\tFrequencies\tFraction\tPercentage"
   mapM_ (printFreq runs) freqs
 
 printFreq :: Int -> (Integer, Int) -> IO ()
-printFreq runs (rounds, freq) =
+printFreq runs (rounds, freq) = do
+  let frac = (fromIntegral freq) / (fromIntegral runs)
   putStrLn $ show rounds ++ "\t\t" ++
              show freq ++ "\t\t" ++
-             show (100 * (fromIntegral freq) / (fromIntegral runs)) ++ "%"
+             showFFloat (Just 3) frac "\t\t" ++ 
+             showFFloat (Just 0) (frac * 100) "%"
 
-experiment :: StdGen -> Int -> Integer -> [Integer]
-experiment g runs crashLimit = map gameRound games
-                               where games = evalState (replicateM runs (simulateGame crashLimit mkGame)) g
+runExperiment :: StdGen -> Int -> GameSettings -> [Integer]
+runExperiment g runs settings = map gameRound games
+                               where games = evalState (replicateM runs (simulateGame settings mkGame)) g
 
-simulateGame :: Integer -> GameInfo -> State StdGen GameInfo
-simulateGame crashLimit gameInfo = do
+simulateGame :: GameSettings -> GameInfo -> State StdGen GameInfo
+simulateGame settings gameInfo = do
+  let crashLimit = settingsCrashLimit settings
   let gameRound' = 1 + gameRound gameInfo
   crashesThisTurn <- newCrashes (gameDanger gameInfo)
   let gameCrashes' = crashesThisTurn + gameCrashes gameInfo
+  let gameDanger'  = if settingsDangerIncrease settings then 1 + (gameDanger gameInfo) else (gameDanger gameInfo)
   let gameInfo' = gameInfo {
-        gameRound = gameRound',
-        gameCrashes = gameCrashes'
+        gameRound   = gameRound',
+        gameCrashes = gameCrashes',
+        gameDanger  = gameDanger'
       }
   if gameCrashes' >= crashLimit
     then return gameInfo'
-    else simulateGame crashLimit gameInfo'
+    else simulateGame settings gameInfo'
   
 newCrashes :: Integer -> State StdGen Integer
 newCrashes danger = do
